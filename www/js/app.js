ad484cff4761214e4004ae2a76c9174a4cceb5ef
yusdesign <swiftcase@gmail.com>
2026-07-22 06:24:48 +0800

    Paths, 🛝

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
-import { GedcomParser } from '/js/GedcomParser.js';
-import { GedcomConverter } from '/js/GedcomConverter.js';
-import { MigrationValidator } from '/js/MigrationValidator.js';
-import { FilePicker } from '/js/capacitor/FilePicker.js';
+import { GedcomParser } from './GedcomParser.js';
+import { GedcomConverter } from './GedcomConverter.js';
+import { MigrationValidator } from './MigrationValidator.js';
+import { FilePicker } from './capacitor/FilePicker.js';
 
 console.log('=== GEDParse APP START ===');
 console.log('📱 Capacitor:', typeof Capacitor !== 'undefined' ? '✅' : '❌');
