f25fed52ca0fe32a9dbe79e67f7c4032a2638914
yusdesign <swiftcase@gmail.com>
2026-07-22 14:03:51 +0800

    Paths, 🛝

--- www/js/globals.js
+++ www/js/globals.js
@@ -1,12 +1,12 @@
 // Загружаем все классы
-import { GedcomField } from '../GedcomField.js';
-import { GedcomRecord } from '../GedcomRecord.js';
-import { GedcomParser } from '../GedcomParser.js';
-import { GedcomConverter } from '../GedcomConverter.js';
-import { MigrationValidator } from '../MigrationValidator.js';
-import { DateUtils } from '../DateUtils.js';
-import { TableRenderer } from '../TableRenderer.js';
-import { FilePicker } from '../capacitor/FilePicker.js';
+import { GedcomField } from 'GedcomField.js';
+import { GedcomRecord } from 'GedcomRecord.js';
+import { GedcomParser } from 'GedcomParser.js';
+import { GedcomConverter } from 'GedcomConverter.js';
+import { MigrationValidator } from 'MigrationValidator.js';
+import { DateUtils } from 'DateUtils.js';
+import { TableRenderer } from 'TableRenderer.js';
+import { FilePicker } from 'capacitor/FilePicker.js';
 
 // Делаем их глобальными
 window.GedcomField = GedcomField;
